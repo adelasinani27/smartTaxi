436b42092dcc8842c502412e525276ff1c9d887d
Adela Sinani <adelasinani27@gmail.com>
2025-12-09 22:30:09 +0000

    .

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,66 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast) {
+    "use strict";
+
+    return Controller.extend("taxi.manual.controller.Detail", {
+        
+        onInit: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteDetail").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var sRideID = oEvent.getParameter("arguments").rideId;
+            var that = this;
+            
+            // Get the OData V2 model
+            var oDataModel = this.getOwnerComponent().getModel();
+            
+            // Build the path for reading a single TaxiRide
+            var sPath = "/TaxiRide(RideID='" + sRideID + "',IsActiveEntity=true)";
+            
+            // Read the specific taxi ride
+            oDataModel.read(sPath, {
+                success: function (oData) {
+                    console.log("Ride details loaded:", oData);
+                    
+                    // Create JSON model with the data
+                    var oDetailModel = new JSONModel(oData);
+                    that.getView().setModel(oDetailModel, "detailModel");
+                    
+                    MessageToast.show("Ride details loaded");
+                },
+                error: function (oError) {
+                    console.error("Error loading ride details:", oError);
+                    MessageToast.show("Error loading ride details");
+                }
+            });
+        },
+
+        onNavBack: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteTaxiRide");
+        },
+
+        onOpenRouteInMaps: function () {
+            var oModel = this.getView().getModel("detailModel");
+            var sPickup = oModel.getProperty("/PickupAddress");
+            var sDropoff = oModel.getProperty("/DropoffAddress");
+
+            if (!sPickup || !sDropoff) {
+                MessageToast.show("Pickup or dropoff address is missing.");
+                return;
+            }
+
+            var sUrl = "https://www.google.com/maps/dir/?api=1" +
+                "&origin=" + encodeURIComponent(sPickup) +
+                "&destination=" + encodeURIComponent(sDropoff);
+
+            window.open(sUrl, "_blank");
+            MessageToast.show("Opening route in Google Maps...");
+        }
+    });
+});
--- webapp/controller/TaxiRide.controller.js
+++ webapp/controller/TaxiRide.controller.js
@@ -1,10 +1,83 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Sorter",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Sorter, MessageToast, Filter, FilterOperator) {
     "use strict";
 
-    return Controller.extend("taxi.manual.taxiui5.controller.TaxiRide", {
-        onInit() {
+    return Controller.extend("taxi.manual.controller.TaxiRide", {
+        
+        onInit: function () {
+            // Create a JSON model to hold the taxi ride data
+            var oTaxiJSONModel = new JSONModel();
+            var that = this;
+
+            // Get the OData V2 model from the component
+            var oDataModel = this.getOwnerComponent().getModel();
+            
+            // Path to TaxiRide entity set
+            var sPath = "/TaxiRide";
+
+            // Read data from OData V2 service
+            oDataModel.read(sPath, {
+                sorters: [new Sorter("RideID", false)],
+                success: function (oResponse) {
+                    console.log("Taxi rides loaded:", oResponse);
+                    
+                    // Set data to JSON model
+                    oTaxiJSONModel.setData(oResponse.results);
+                    
+                    // Bind model to view
+                    that.getView().setModel(oTaxiJSONModel, "taxiModel");
+                    
+                    MessageToast.show("Loaded " + oResponse.results.length + " taxi rides");
+                },
+                error: function (oError) {
+                    console.error("Error loading taxi rides:", oError);
+                    MessageToast.show("Error loading taxi rides");
+                }
+            });
+        },
+
+        onRidePress: function (oEvent) {
+            // Get the selected ride
+            var oItem = oEvent.getSource();
+            var oContext = oItem.getBindingContext("taxiModel");
+            var oRide = oContext.getObject();
+            
+            console.log("Selected ride:", oRide);
+            
+            // Navigate to detail page
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteDetail", {
+                rideId: oRide.RideID
+            });
+        },
+
+        onSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oTable = this.byId("taxiRideTable");
+            var oBinding = oTable.getBinding("items");
+            
+            if (sQuery) {
+                var aFilters = [
+                    new Filter({
+                        filters: [
+                            new Filter("RideID", FilterOperator.Contains, sQuery),
+                            new Filter("CustomerName", FilterOperator.Contains, sQuery),
+                            new Filter("PickupAddress", FilterOperator.Contains, sQuery),
+                            new Filter("DropoffAddress", FilterOperator.Contains, sQuery)
+                        ],
+                        and: false
+                    })
+                ];
+                oBinding.filter(aFilters);
+            } else {
+                oBinding.filter([]);
+            }
         }
     });
 });
